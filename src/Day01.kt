42c121c9fd0db761cb9a233e67a7cdee5d02f695
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2023-12-06 21:48:57 +0530

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
+import java.util.Comparator
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, s ->
+            acc + s.first { it.isDigit() }.digitToInt() * 10 + s.last { it.isDigit() }.digitToInt()
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digits = arrayOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        return input.fold(0) { acc, s ->
+            val numbers = arrayListOf<Int>()
+            s.forEachIndexed { index, c ->
+                if (c.isDigit()) {
+                    numbers.add(c.digitToInt())
+                }
+
+                digits.forEachIndexed sub@ { n, digit ->
+                    if (s.substring(index).startsWith(digit)) {
+                        numbers.add(n)
+                        return@sub
+                    }
+                }
+            }
+
+            acc + numbers.first() * 10 + numbers.last()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
